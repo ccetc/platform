9f91b9ac315ddb0f27003f64f56839f9aa43ff4d
Greg Kops <greg@thinktopography.com>
2016-11-07 23:16:54 -0500

    flattening authentication middleware

--- src/server/middleware/authentication.js
+++ src/server/middleware/authentication.js
@@ -1,8 +1,8 @@
 import jwt from 'jwt-simple'
-import config from '../../../config/platform'
+import config from '../../services/config'
 import User from '../../platform/models/user'
 
-const secret = config[process.env.NODE_ENV].secret
+const secret = config.secret
 
 const decode = (token) => {
   try {
@@ -13,49 +13,48 @@ const decode = (token) => {
 }
 
 export default (req, res, next) => {
+
+  const header = req.header('Authorization')
+  if(!header) {
+    return res.json({ message: 'nonexistant token' }).status(401)
+  }
+
+  const matches = header.match('Bearer (.*)')
+  if(!matches) {
+    return res.json({ message: 'malformed token' }).status(401)
+  }
+
+  const data = decode(matches[1])
+  if(!data) {
+    return res.json({ message: 'invalid token' }).status(401)
+  }
+
   const timestamp = Math.round(new Date() / 1000)
+  const two_weeks = 60 * 60 * 24 * 7 * 2
+  if(data.timestamp <= timestamp - two_weeks) {
+    return res.json({ message: 'expired token' }).status(401)
+  }
 
-  if(req.header('Authorization')) {
+  User.where({ id: data.user_id }).fetch().then(user => {
 
-    const header = req.header('Authorization')
-    const matches = header.match('Bearer (.*)')
-
-    if(!matches) {
-      return res.json({ message: 'malformed token' }).status(401)
+    if(!user) {
+      return res.json({ message: 'cannot find user' }).status(401)
     }
 
-    const data = decode(matches[1])
-    if(!data) {
-      return res.json({ message: 'invalid token' }).status(401)
-    }
-
-    const two_weeks = 60 * 60 * 24 * 7 * 2
-    if(data.timestamp <= timestamp - two_weeks) {
+    const logged_out_at = user.get('logged_out_at')
+    if(logged_out_at && data.timestamp <= Math.round(logged_out_at / 1000)) {
       return res.json({ message: 'expired token' }).status(401)
     }
 
-    User.where({ id: data.user_id }).fetch({ require: true }).then(user => {
+    if(req.path === '/refresh') {
+      const encoded = jwt.encode({ timestamp, user_id: user.id }, secret)
+      return res.json({ token: encoded }).status(200)
+    }
 
-      const logged_out_at = user.get('logged_out_at')
-      if(logged_out_at && data.timestamp <= Math.round(logged_out_at / 1000)) {
-        return res.json({ message: 'expired token' }).status(401)
-      }
+    next()
 
-      if(req.path === '/refresh') {
-        const encoded = jwt.encode({ timestamp, user_id: user.id }, secret)
-        return res.json({ token: encoded }).status(200)
-      }
+    return null
 
-      next()
-
-      return null
-
-    }).catch(err => {
-      return res.json({ message: 'cannot find user' }).status(401)
-    })
-
-  } else {
-    return res.json({ message: 'nonexistant token' }).status(401)
-  }
+  })
 
 }
