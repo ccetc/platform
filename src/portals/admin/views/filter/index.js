d2483bd9bf03ac8d9e7750f2e9e506794bea6605
Greg Kops <greg@thinktopography.com>
2016-12-29 10:38:06 -0800

    implementing responsive tray

--- src/portals/admin/views/filter/index.js
+++ src/portals/admin/views/filter/index.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 class Filter extends React.Component {
 
+  static contextTypes = {
+    tray: React.PropTypes.object
+  }
 
   render() {
+    const fields = [
+      { label: 'Item 1' },
+      { label: 'Item 2' },
+      { label: 'Item 3' },
+      { label: 'Item 4' },
+      { label: 'Item 5' },
+      { label: 'Item 6' },
+      { label: 'Item 7' },
+      { label: 'Item 8' },
+      { label: 'Item 9' }
+    ]
     return (
       <div className="filter">
         <div className="filter-header">
@@ -14,45 +28,37 @@ class Filter extends React.Component {
           <div className="filter-header-title">
             All Filters
           </div>
-          <div className="filter-header-next">
+          <div className="filter-header-next" onClick={ this._handleDone.bind(this) }>
             Done
           </div>
         </div>
         <div className="filter-body">
-          <div className="filter-item">
-            Item 1
-            <i className="chevron right icon" />
-          </div>
-          <div className="filter-item">
-            Item 2
-            <i className="chevron right icon" />
-          </div>
-          <div className="filter-item">
-            Item 3
-            <i className="chevron right icon" />
-          </div>
-          <div className="filter-item">
-            Item 4
-            <i className="chevron right icon" />
-          </div>
-          <div className="filter-item">
-            Item 5
-            <i className="chevron right icon" />
-          </div>
-          <div className="filter-item">
-            Item 6
-            <i className="chevron right icon" />
-          </div>
-          <div className="filter-item">
-            Item 7
-            <i className="chevron right icon" />
-          </div>
+          { fields.map((field, index) => {
+            return (
+              <div key={`field_${index}`} className="filter-item" onClick={ this._handleChoose.bind(this, index) }>
+                {field.label}
+                <i className="chevron right icon" />
+              </div>
+            )
+          }) }
         </div>
-        <div className="filter-footer">
+        <div className="filter-footer" onClick={ this._handleReset.bind(this) }>
           Reset Filter
         </div>
       </div>
     )
+  }
+
+  _handleDone() {
+    this.context.tray.close()
+  }
+
+  _handleReset() {
+    console.log('reset')
+  }
+
+  _handleChoose(index) {
+    console.log('choose', index)
   }
 
 }
