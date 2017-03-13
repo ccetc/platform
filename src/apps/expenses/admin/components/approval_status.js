22ef307d03460334679c4640f5fbb8492f57be91
Greg Kops <greg@thinktopography.com>
2017-03-13 14:51:45 -0400

    must return null

--- src/apps/expenses/admin/components/approval_status.js
+++ src/apps/expenses/admin/components/approval_status.js
@@ -4,10 +4,12 @@ export const ApprovalBadge = props => {
   if(props.is_approved === true) return <span className="approved">APPROVED</span>
   if(props.is_approved === false) return <span className="rejected">REJECTED</span>
   if(props.is_submitted && props.is_approved === null) return <span className="unreviewed">UNREVIEWED</span>
+  return null
 }
 
 export const ApprovalAlert = props => {
   if(props.is_approved === true) return <div className="ui center aligned green inverted segment">This expense has been approved</div>
   if(props.is_approved === false) return <div className="ui center aligned red inverted segment">This expense has been rejected</div>
   if(props.is_submitted && props.is_approved === null) return <div className="ui center aligned blue inverted segment">This expense is pending review</div>
+  return null
 }
