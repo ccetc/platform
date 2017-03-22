029869c973efc8573c9a3da73e5b987e167b3806
Greg Kops <greg@thinktopography.com>
2017-03-21 21:47:05 -0400

    fixing notifications dialog

--- src/admin/views/notifications/feed.js
+++ src/admin/views/notifications/feed.js
@@ -25,47 +25,49 @@ export class Feed extends React.Component {
     const { records, status } = this.props
     if(records.length > 0) {
       return (
-        <div className="chrome-feed">
-          <div className="chrome-feed-items">
-            { records.map((item, index) => {
-              let story = `<span class="chrome-feed-item-subject">${item.subject.full_name}</span> ${item.story.text}`
-              if(item.object1) {
-                story = story.replace('{object1}', `${item.object1.description} <span class="chrome-feed-item-object">${item.object1.text}</span>`)
-              }
-              if(item.object2) {
-                story = story.replace('{object2}', `${item.object2.description} <span class="chrome-feed-item-object">${item.object2.text}</span>`)
-              }
-              let classes = ['chrome-feed-item']
-              if(item.is_read !== undefined && !item.is_read) {
-                classes.push('unread')
-              }
-              return (
-                <a key={`item_${index}`} className={classes.join(' ')} onClick={this._onClick.bind(this, item.url)}>
-                  <div className="chrome-feed-item-avatar">
-                    <Avatar user={ item.subject } width="40" />
-                  </div>
-                  <div className="chrome-feed-item-details">
-                    <div className="chrome-feed-item-story">
-                      <span dangerouslySetInnerHTML={{__html: story }} />
+        <div className="collection-layout">
+          <div className="chrome-feed">
+            <div className="chrome-feed-items">
+              { records.map((item, index) => {
+                let story = `<span class="chrome-feed-item-subject">${item.subject.full_name}</span> ${item.story.text}`
+                if(item.object1) {
+                  story = story.replace('{object1}', `${item.object1.description} <span class="chrome-feed-item-object">${item.object1.text}</span>`)
+                }
+                if(item.object2) {
+                  story = story.replace('{object2}', `${item.object2.description} <span class="chrome-feed-item-object">${item.object2.text}</span>`)
+                }
+                let classes = ['chrome-feed-item']
+                if(item.is_read !== undefined && !item.is_read) {
+                  classes.push('unread')
+                }
+                return (
+                  <a key={`item_${index}`} className={classes.join(' ')} onClick={this._onClick.bind(this, item.url)}>
+                    <div className="chrome-feed-item-avatar">
+                      <Avatar user={ item.subject } width="40" />
                     </div>
-                    <div className="chrome-feed-item-timestamp">
-                      <div className="chrome-feed-item-app-icon">
-                        <i className={`${ item.app.icon } icon`} />
+                    <div className="chrome-feed-item-details">
+                      <div className="chrome-feed-item-story">
+                        <span dangerouslySetInnerHTML={{__html: story }} />
                       </div>
-                      { moment(item.created_at).calendar(null, { sameDay: '[Today]', lastDay: '[Yesterday]', lastWeek: '[Last] dddd', sameElse: 'MMMM Do' }) } at { moment(item.created_at).format('h:mm a') }
+                      <div className="chrome-feed-item-timestamp">
+                        <div className="chrome-feed-item-app-icon">
+                          <i className={`${ item.app.icon } icon`} />
+                        </div>
+                        { moment(item.created_at).calendar(null, { sameDay: '[Today]', lastDay: '[Yesterday]', lastWeek: '[Last] dddd', sameElse: 'MMMM Do' }) } at { moment(item.created_at).format('h:mm a') }
+                      </div>
                     </div>
-                  </div>
-                </a>
-              )
-            })}
-          </div>
-          { status === 'loading' &&
-            <div className="chrome-infinite-loader">
-              <div className="ui active inverted dimmer">
-                <div className="ui small loader"></div>
-              </div>
+                  </a>
+                )
+              })}
             </div>
-          }
+            { status === 'loading' &&
+              <div className="chrome-infinite-loader">
+                <div className="ui active inverted dimmer">
+                  <div className="ui small loader"></div>
+                </div>
+              </div>
+            }
+          </div>
         </div>
       )
     } else if(status === 'completed' && records.length === 0) {
